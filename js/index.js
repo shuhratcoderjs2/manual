b062771c34793d9a2375246071397cb3dfe10fb6
shuhratcoderjs2 <shuhratcoderjs@gmail.com>
2022-03-28 18:50:36 +0500

    hj

--- js/index.js
+++ js/index.js
@@ -8,3 +8,36 @@ $burger.addEventListener('click', ()=>{
 $closeBurger.addEventListener('click', ()=>{
     $navbar.classList.remove("navbar__active")
 })
+
+// animation rasm
+window.onscroll = function() {g()};
+
+function g(){
+  myFunction()
+  myFunction1()
+}
+  let img = document.querySelectorAll(".myImg")
+  let text = document.querySelectorAll(".info__text")
+  function myFunction() {
+      let height = window.innerHeight *2.2
+
+          img.forEach(item =>{
+              if (document.body.scrollTop > height || document.documentElement.scrollTop > height) {
+                  item.classList.add ("slideUp")
+              }
+              height += 450
+          })
+         
+  }
+
+  function myFunction1(){
+      let height = window.innerHeight / 2
+
+      text.forEach(item =>{
+              if (document.body.scrollTop > height || document.documentElement.scrollTop > height) {
+                  item.classList.add("slideUp1")
+              }
+              height += 450
+          })
+  
+  }
